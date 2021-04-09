cc99c16d7b56fd42bc6a0dc5f0212f8d77fc88d3
camalmborg <malmborg@bu.edu>
2021-04-09 15:28:12 -0400

    changes to extractNOAA and milestone 6

--- extractNOAA.R
+++ extractNOAA.R
@@ -6,13 +6,34 @@ siteID= c('BART','CLBJ','DELA','GRSM','HARV','SCBI','STEI','UKFS')
 base='Downloads/noaa/NOAAGEFS_1hr/'
 mid='/00/'
 date=(Sys.Date()-1)
-directory=paste0(base,siteID,'/',date,mid)
-files=dir_ls(directory)
+
+#old code: makes df1
+#directory=paste0(base,siteID,'/',date,mid)
+#files=dir_ls(directory)
+
+# l.files=length(files)
+# df1 <-as.data.frame(matrix(nrow=1000,ncol=385))
+# for (i in  1:l.files){
+#   cat=nc_open(files[i])
+#   df1[i,]=ncvar_get(cat,'air_temperature')
+# }
+# df1=na.omit(df1)
+
+#new code as of 4/12, makes df1 into list with NOAA data from all sites
+df1 <-list()
+for (s in seq_along(siteID)){
+  directory=paste0(base,siteID[s],'/',date,mid)
+  files=dir_ls(directory)
 
 l.files=length(files)
-df1 <-as.data.frame(matrix(nrow=1000,ncol=385))
-for (i in  1:l.files){
-  cat=nc_open(files[i])
-  df1[i,]=ncvar_get(cat,'air_temperature')
+df1[[s]] <-as.data.frame(matrix(nrow=l.files,ncol=385))
+  for (i in  1:l.files){
+    cat=nc_open(files[i])
+    df1[[s]][i,]=ncvar_get(cat,'air_temperature')
+    nc_close(cat)
+    df1[[s]]=na.omit(df1[[s]])
+  }
+#df1[[s]]=na.omit(df1[[s]])
 }
-df1=na.omit(df1)
+
+names(df1)#=siteID
